156156baecf6b819b219d3e3bcfb1581686df749
Zabegailo1990 <zvv56739@gmail.com>
2025-03-04 00:20:51 +0300

    updated

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -30,3 +30,35 @@ $(document).ready(function () {
       event.stopPropagation();
   });
 });
+
+
+$(document).ready(function () {
+    $("body").on("click", ".check-proxy__select", function (e) {
+        e.stopPropagation(); // Останавливаем всплытие, чтобы не срабатывало на body
+        $(".check-proxy__options").removeClass("check-proxy__options--active"); // Закрываем все открытые списки
+        $(this).find(".check-proxy__options").addClass("check-proxy__options--active");
+    });
+
+    // Выбор элемента из списка
+    $("body").on("click", ".check-proxy__value", function (e) {
+        e.stopPropagation(); // Останавливаем всплытие
+        let $select = $(this).closest(".check-proxy__select");
+        let $name = $select.find(".check-proxy__name");
+        
+        // Обновляем текст в блоке
+        $name.text($(this).text());
+
+        // Убираем старый selected и добавляем к текущему
+        $select.find(".check-proxy__value").removeClass("check-proxy__value--selected");
+        $(this).addClass("check-proxy__value--selected");
+
+        // Закрываем список
+        $select.find(".check-proxy__options").removeClass("check-proxy__options--active");
+    });
+
+    // Закрытие при клике вне элемента
+    $(document).on("click", function () {
+        $(".check-proxy__options").removeClass("check-proxy__options--active");
+    });
+});
+
