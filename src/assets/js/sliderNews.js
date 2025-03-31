9966ff221466efe40a141a4af078e0eb6aabb2e9
Zabegailo1990 <zvv56739@gmail.com>
2025-03-31 21:14:39 +0300

    Update

--- src/assets/js/sliderNews.js
+++ src/assets/js/sliderNews.js
@@ -1,9 +1,41 @@
 $(document).ready(function(){
-    $('.slider-news__slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        prevArrow: $('.slider-news__prev'),
-        nextArrow: $('.slider-news__next'),
-        infinite: false,
+    // Проверяем ширину экрана при загрузке
+    if (window.innerWidth >= 1440) {
+        initSlickSlider();
+    }
+
+    // Также проверяем при изменении размера окна
+    $(window).on('resize', function() {
+        if (window.innerWidth >= 1440) {
+            // Если слайдер еще не инициализирован - инициализируем
+            if (!$('.slider-news__slider').hasClass('slick-initialized')) {
+                initSlickSlider();
+            }
+        } else {
+            // Если слайдер инициализирован - уничтожаем его
+            if ($('.slider-news__slider').hasClass('slick-initialized')) {
+                $('.slider-news__slider').slick('unslick');
+            }
+        }
     });
+
+    function initSlickSlider() {
+        $('.slider-news__slider').slick({
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            prevArrow: $('.slider-news__prev'),
+            nextArrow: $('.slider-news__next'),
+            infinite: false,
+            responsive: [
+                {
+                    breakpoint: 1440,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                        infinite: false,
+                    }
+                }
+            ]
+        });
+    }
 });
