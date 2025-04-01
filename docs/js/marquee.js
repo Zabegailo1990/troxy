f42c7cb3a3cd3000ca3f4b10d86796d1c6a7a216
Zabegailo1990 <zvv56739@gmail.com>
2025-04-01 03:26:43 +0300

    ++

--- docs/js/marquee.js
+++
@@ -1,1 +0,0 @@
-let cont=document.querySelector(".marquee__slider");gsap.to(".marquee__card",{ease:"none",x:()=>-(cont.scrollWidth-window.innerWidth),scrollTrigger:{trigger:cont,pin:cont,start:"center center",end:()=>"+="+(cont.scrollWidth-window.innerWidth),scrub:!0,invalidateOnRefresh:!0,markers:!0}});
