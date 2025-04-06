90d326b8aff004364ba174c07300987a7ae0d7d7
Zabegailo1990 <zvv56739@gmail.com>
2025-04-06 12:18:17 +0300

    Update

--- src/assets/js/benefitCards.js
+++ src/assets/js/benefitCards.js
@@ -3,18 +3,20 @@ const mm = gsap.matchMedia();
 const tl = gsap.timeline({
     scrollTrigger: {
         scrub: 1,
-        trigger: ".home__benefits",
+        trigger: ".benefits",
         start: "top top",
-        end: "4000px",
+        end: "4000",
         pin: true,
     }
 });
+
+const animatedCards = document.querySelectorAll(".benefits__card-benefit:not(:first-child)");
 
 // Используем matchMedia только для выбора X
 mm.add("(min-width: 1441px)", () => tl.vars.x = "-100vw"); 
 mm.add("(max-width: 1440px)", () => tl.vars.x = "0"); 
 
-tl.fromTo(".home__card-benefit", 
+tl.fromTo(animatedCards, 
     { y: "100vh", x: tl.vars.x }, // X адаптируется
     { y: 0, x: 0, duration: 3, stagger: 2, ease: "power1.out" }
 );
