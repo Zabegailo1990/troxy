cbaeb29f30c155ffb9de01381b51c87089607ff5
Zabegailo1990 <zvv56739@gmail.com>
2025-04-07 08:45:23 +0300

    Update

--- docs/js/sliderHome.js
+++ docs/js/sliderHome.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$(".slider-home__carousel").slick({slidesToShow:1,slidesToScroll:1,prevArrow:$(".slider-home__button--prev"),nextArrow:$(".slider-home__button--next"),infinite:!1})});
+$(document).ready(function(){$(".slider-home__carousel").slick({slidesToShow:1,slidesToScroll:1,prevArrow:$(".c-panel__button--prev"),nextArrow:$(".c-panel__button--next"),infinite:!1})});
--- src/assets/js/sliderHome.js
+++ src/assets/js/sliderHome.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.slider-home__carousel').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        prevArrow: $('.slider-home__button--prev'),
-        nextArrow: $('.slider-home__button--next'),
+        prevArrow: $('.c-panel__button--prev'),
+        nextArrow: $('.c-panel__button--next'),
         infinite: false,
     });
 });
