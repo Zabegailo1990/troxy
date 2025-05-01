b255ae3d25644a1e2812cf9b544041b576904aa9
Zabegailo1990 <zvv56739@gmail.com>
2025-05-01 04:52:30 +0300

    updated

--- src/assets/js/c-marquee.js
+++ src/assets/js/c-marquee.js
@@ -1,54 +1,54 @@
-const slides = gsap.utils.toArray('.c-marquee__card');
-const totalSlides = slides.length; 
+const isTablet = window.matchMedia('(min-width: 780px)').matches;
 
-const swiper = new Swiper('.c-marquee', {
-    effect: 'coverflow',
-    slidesPerView: 'auto',
-    centeredSlides: true,
-    loop: true,
-    speed: 400,
-    coverflowEffect: {
-        rotate: 0,
-        stretch: -30,
-        depth: 100,
-        modifier: 1.3,
-        scale: 0.95,
-        slideShadows: false,
-    },
-    on: {
-        init: function() {
-            updateSlideOpacity();
+if (isTablet) {
+
+    const slides = gsap.utils.toArray('.c-marquee__card');
+    const totalSlides = slides.length;
+
+    const swiper = new Swiper('.c-marquee', {
+        effect: 'coverflow',
+        slidesPerView: 'auto',
+        centeredSlides: true,
+        loop: true,
+        speed: 400,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: -30,
+            depth: 100,
+            modifier: 1.3,
+            scale: 0.95,
+            slideShadows: false,
         },
-        slideChangeTransitionStart: function() {
-            updateSlideOpacity();
-        },
-        slideChangeTransitionEnd: function() {
-            updateSlideOpacity();
+        on: {
+            init() {
+                updateSlideEffects();
+            },
+            slideChangeTransitionStart: updateSlideEffects,
+            slideChangeTransitionEnd: updateSlideEffects
         }
-    }
-});
-
-function updateSlideOpacity() {
-    slides.forEach(slide => {
-        const currentSlide = slide.classList.contains('swiper-slide-active');
-        gsap.to(slide, { 
-            opacity: currentSlide ? 1 : 0.6,
-            duration: 0.3,
-        });
     });
-}
 
-gsap.to({}, {
-    scrollTrigger: {
+    function updateSlideEffects() {
+        slides.forEach(slide => {
+            const isActive = slide.classList.contains('swiper-slide-active');
+                gsap.to(slide, {
+                opacity: isActive ? 1 : 0.6,
+                duration: 0.3
+            });
+        });
+    }
+
+    gsap.to({}, {
+        scrollTrigger: {
         trigger: ".p-home__section-marquee",
         start: "top 90",
         end: "+=8000",
         scrub: 2,
         pin: true,
-        onUpdate: self => {
-        const progress = self.progress;
-        const slideIndex = Math.floor(progress * totalSlides);
-        swiper.slideToLoop(slideIndex);
-        }
-    }
-});
+        onUpdate(self) {
+            const progress = Math.min(0.999, self.progress);
+            const slideIndex = Math.floor(progress * totalSlides);
+            swiper.slideToLoop(slideIndex);
+        }}
+    });
+}
