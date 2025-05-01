abb13cf34c3497e1f4b16d5a3b136e10b576c9a8
Zabegailo1990 <zvv56739@gmail.com>
2025-05-01 04:28:24 +0300

    updated

--- src/assets/js/c-marquee.js
+++ src/assets/js/c-marquee.js
@@ -1,60 +1,60 @@
-// 1. Инициализация Swiper
+const slides = gsap.utils.toArray('.c-marquee__inner .swiper-slide');
+const totalSlides = slides.length; 
+
 const swiper = new Swiper('.c-marquee', {
-  effect: 'coverflow',
-  slidesPerView: 'auto',
-  centeredSlides: true,
-  loop: true,
-  speed: 400,
-  coverflowEffect: {
-    rotate: 0,
-    stretch: -30,
-    depth: 100,
-    modifier: 1.3,
-    scale: 0.95,
-    slideShadows: false,
-  },
-  on: {
-    init: function() {
-      updateSlideOpacity();
+    effect: 'coverflow',
+    slidesPerView: 'auto',
+    centeredSlides: true,
+    loop: true,
+    speed: 400,
+    coverflowEffect: {
+        rotate: 0,
+        stretch: -30,
+        depth: 100,
+        modifier: 1.3,
+        scale: 0.95,
+        slideShadows: false,
     },
-    slideChangeTransitionStart: function() {
-      updateSlideOpacity();
-    },
-    slideChangeTransitionEnd: function() {
-      updateSlideOpacity();
+    on: {
+        init: function() {
+            updateSlideOpacity();
+        },
+        slideChangeTransitionStart: function() {
+            updateSlideOpacity();
+        },
+        slideChangeTransitionEnd: function() {
+            updateSlideOpacity();
+        }
     }
-  }
 });
 
 function updateSlideOpacity() {
-  const slides = gsap.utils.toArray('.c-marquee__inner .swiper-slide');
-  
-  slides.forEach(slide => {
-    // Проверяем, является ли слайд активным
-    if (slide.classList.contains('swiper-slide-active')) {
-      gsap.to(slide, { opacity: 1, duration: 0.3 });
-    } else {
-      gsap.to(slide, { opacity: 0.6, duration: 0.3 }); // Можно изменить значение opacity
-    }
-  });
+    slides.forEach(slide => {
+        if (slide.classList.contains('swiper-slide-active')) {
+            gsap.to(slide, { 
+                opacity: 1, 
+                duration: 0.3 
+            });
+        } else {
+            gsap.to(slide, { 
+                opacity: 0.6, 
+                duration: 0.3 
+            }); 
+        }
+    });
 }
 
-// 3. Настройка GSAP ScrollTrigger
-gsap.registerPlugin(ScrollTrigger);
-
-const totalSlides = document.querySelectorAll('.c-marquee__inner .swiper-slide').length;
-
 gsap.to({}, {
-  scrollTrigger: {
-    trigger: ".p-home__section-marquee",
-    start: "top 90",
-    end: "+=8000",
-    scrub: 2,
-    pin: true,
-    onUpdate: self => {
-      const progress = Math.min(0.999, self.progress);
-      const slideIndex = Math.floor(progress * totalSlides);
-      swiper.slideToLoop(slideIndex);
+    scrollTrigger: {
+        trigger: ".p-home__section-marquee",
+        start: "top 90",
+        end: "+=8000",
+        scrub: 2,
+        pin: true,
+        onUpdate: self => {
+        const progress = self.progress;
+        const slideIndex = Math.floor(progress * totalSlides);
+        swiper.slideToLoop(slideIndex);
+        }
     }
-  }
 });
